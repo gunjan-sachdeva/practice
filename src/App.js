7bfb18fd62ec11858c71f72ae7c427a29d50b84e
gunjan-sachdeva <58691180+gunjan-sachdeva@users.noreply.github.com>
2022-11-29 12:13:15 +0530

    dev branch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
       <h1>welcome to the world of React Js</h1>
       <p>Hello React Js</p>
+      <Home />
     </div>
   );
 }
